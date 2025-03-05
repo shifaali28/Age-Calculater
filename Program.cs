4f996f5960e1dbf46a716c21a3c57510eb897c56
WIN 10 PRO <WIN 10 PRO@DESKTOP-KHL553M>
2025-03-05 13:23:58 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("===== AGE CALCULATOR =====");
+                Console.Write("Enter your birthdate (YYYY-MM-DD) or type 'exit' to quit: ");
+
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "exit")
+                {
+                    Console.WriteLine("Exiting... Have a great day!");
+                    break;
+                }
+
+                if (DateTime.TryParse(input, out DateTime birthdate))
+                {
+                    int age = CalculateAge(birthdate);
+                    Console.WriteLine($"You are {age} years old.");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid date format! Please enter a valid date (YYYY-MM-DD).");
+                }
+
+                Console.WriteLine("\nPress Enter to calculate again...");
+                Console.ReadLine();
+            }
+        }
+
+        static int CalculateAge(DateTime birthdate)
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - birthdate.Year;
+
+            // Adjust if the birthday has not occurred yet this year
+            if (birthdate.Date > today.AddYears(-age))
+            {
+                age--;
+            }
+
+            return age;
+        }
+    }
+
+
+
+
+
+
+
+
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Task")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Task")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("838868a3-aa76-48a3-8db6-8b56ea4519aa")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
